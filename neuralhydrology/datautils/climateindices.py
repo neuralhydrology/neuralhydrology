4057ce16b3223f40c150f2b1d82644108efae089
Martin Gauch <15731649+gauchm@users.noreply.github.com>
2020-11-04 14:39:07 +0100

    Refactor climateindices (#49)
    climateindices was lagging behind some recent codebase changes. Fixed
    some calculation issues, added a method to calculate indices for
    non-CAMELS data.

--- neuralhydrology/datautils/climateindices.py
+++ neuralhydrology/datautils/climateindices.py
@@ -9,22 +9,26 @@ import pandas as pd
 from numba import njit
 from tqdm import tqdm
 
-from neuralhydrology.datautils import pet, utils
+from neuralhydrology.datasetzoo.camelsus import load_camels_us_forcings, load_camels_us_attributes
+from neuralhydrology.datautils import pet
 
 LOGGER = logging.getLogger(__name__)
 
 
-def calculate_dyn_climate_indices(data_dir: Path,
-                                  basins: List[str],
-                                  window_length: int,
-                                  forcings: str,
-                                  output_file: Path = None) -> Dict[str, pd.DataFrame]:
-    """Calculate dynamic climate indices.
+def calculate_camels_us_dyn_climate_indices(data_dir: Path,
+                                         basins: List[str],
+                                         window_length: int,
+                                         forcings: str,
+                                         variable_names: Dict[str, str] = None,
+                                         output_file: Path = None) -> Dict[str, pd.DataFrame]:
+    """Calculate dynamic climate indices for the CAMELS US dataset.
     
-    Compared to the long-term static climate indices included in the CAMELS data set, this function computes the same
+    Compared to the long-term static climate indices included in the CAMELS US data set, this function computes the same
     climate indices by a moving window approach over the entire data set. That is, for each time step, the climate 
     indices are re-computed from the last `window_length` time steps. The resulting dictionary of DataFrames can be
     used with the `additional_feature_files` argument.
+    Unlike in CAMELS, the '_freq' indices will be fractions, not number of days. To compare the values to the ones in
+    CAMELS, they need to be multiplied by 365.25.
     
     Parameters
     ----------
@@ -35,7 +39,11 @@ def calculate_dyn_climate_indices(data_dir: Path,
     window_length : int
         Look-back period to use to compute the climate indices.
     forcings : str
-        Can be e.g. 'daymet' or 'nldas', etc. Must match the folder names in the 'basin_mean_forcing' directory. 
+        Can be e.g. 'daymet' or 'nldas', etc. Must match the folder names in the 'basin_mean_forcing' directory.
+    variable_names : Dict[str, str], optional
+        Mapping of the forcings' variable names, needed if forcings other than DayMet, Maurer, or NLDAS are used.
+        If provided, this must be a dictionary that maps the keys 'prcp', 'tmin', 'tmax', 'srad' to the forcings'
+        respective variable names.
     output_file : Path, optional
         If specified, stores the resulting dictionary of DataFrames to this location as a pickle dump.
 
@@ -45,51 +53,39 @@ def calculate_dyn_climate_indices(data_dir: Path,
         Dictionary with one time-indexed DataFrame per basin. By definition, the climate indices for a given day in the
         DataFrame are computed from the `window_length` previous time steps (including the given day).
     """
-    camels_attributes = utils.load_camels_us_attributes(data_dir=data_dir, basins=basins)
+    camels_attributes = load_camels_us_attributes(data_dir=data_dir, basins=basins)
     additional_features = {}
-    new_columns = [
-        'p_mean_dyn', 'pet_mean_dyn', 'aridity_dyn', 't_mean_dyn', 'frac_snow_dyn', 'high_prec_freq_dyn',
-        'high_prec_dur_dyn', 'low_prec_freq_dyn', 'low_prec_dur_dyn'
-    ]
+
+    if variable_names is None:
+        if forcings.startswith('nldas'):
+            variable_names = {'prcp': 'PRCP(mm/day)', 'tmin': 'Tmin(C)', 'tmax': 'Tmax(C)', 'srad': 'SRAD(W/m2)'}
+        elif forcings.startswith('daymet') or forcings.startswith('maurer'):
+            variable_names = {'prcp': 'prcp(mm/day)', 'tmin': 'tmin(C)', 'tmax': 'tmax(C)', 'srad': 'srad(W/m2)'}
+        else:
+            raise ValueError(f'No predefined variable mapping for {forcings} forcings. Provide one in variable_names.')
+
     for basin in tqdm(basins, file=sys.stdout):
-        df, _ = utils.load_camels_us_forcings(data_dir=data_dir, basin=basin, forcings=forcings)
+        df, _ = load_camels_us_forcings(data_dir=data_dir, basin=basin, forcings=forcings)
         lat = camels_attributes.loc[camels_attributes.index == basin, 'gauge_lat'].values
         elev = camels_attributes.loc[camels_attributes.index == basin, 'elev_mean'].values
-        df["PET(mm/d)"] = pet.get_priestley_taylor_pet(t_min=df["tmin(C)"].values,
-                                                       t_max=df["tmax(C)"].values,
-                                                       s_rad=df["srad(W/m2)"].values,
+        df["PET(mm/d)"] = pet.get_priestley_taylor_pet(t_min=df[variable_names['tmin']].values,
+                                                       t_max=df[variable_names['tmax']].values,
+                                                       s_rad=df[variable_names['srad']].values,
                                                        lat=lat,
                                                        elev=elev,
                                                        doy=df.index.dayofyear.values)
 
-        for col in new_columns:
-            df[col] = np.nan
+        clim_indices = calculate_dyn_climate_indices(df[variable_names['prcp']],
+                                                     df[variable_names['tmax']],
+                                                     df[variable_names['tmin']],
+                                                     df['PET(mm/d)'],
+                                                     window_length=window_length)
 
-        x = np.array([
-            df['prcp(mm/day)'].values, df['srad(W/m2)'].values, df['tmax(C)'].values, df['tmin(C)'].values,
-            df['vp(Pa)'].values, df['PET(mm/d)'].values
-        ]).T
-
-        new_features = _numba_climate_indexes(x, window_length=window_length)
-
-        if np.sum(np.isnan(new_features)) > 0:
+        if np.any(clim_indices.isna()):
             raise ValueError(f"NaN in new features of basin {basin}")
 
-        df.loc[df.index[window_length - 1]:, 'p_mean_dyn'] = new_features[:, 0]
-        df.loc[df.index[window_length - 1]:, 'pet_mean_dyn'] = new_features[:, 1]
-        df.loc[df.index[window_length - 1]:, 'aridity_dyn'] = new_features[:, 2]
-        df.loc[df.index[window_length - 1]:, 't_mean_dyn'] = new_features[:, 3]
-        df.loc[df.index[window_length - 1]:, 'frac_snow_dyn'] = new_features[:, 4]
-        df.loc[df.index[window_length - 1]:, 'high_prec_freq_dyn'] = new_features[:, 5]
-        df.loc[df.index[window_length - 1]:, 'high_prec_dur_dyn'] = new_features[:, 6]
-        df.loc[df.index[window_length - 1]:, 'low_prec_freq_dyn'] = new_features[:, 7]
-        df.loc[df.index[window_length - 1]:, 'low_prec_dur_dyn'] = new_features[:, 8]
-
-        drop_cols = [c for c in df.columns if c not in new_columns]
-
-        df = df.drop(drop_cols, axis=1)
-
-        additional_features[basin] = df
+        clim_indices = clim_indices.reindex(df.index)  # add NaN rows for the first window_length - 1 entries
+        additional_features[basin] = clim_indices
 
     if output_file is not None:
         with output_file.open("wb") as fp:
@@ -99,10 +95,73 @@ def calculate_dyn_climate_indices(data_dir: Path,
     return additional_features
 
 
+def calculate_dyn_climate_indices(precip: pd.Series,
+                                  tmax: pd.Series,
+                                  tmin: pd.Series,
+                                  pet: pd.Series,
+                                  window_length: int,
+                                  raise_nan=False) -> pd.DataFrame:
+    """Calculate dynamic climate indices.
+
+    Compared to the long-term static climate indices included in the CAMELS dataset, this function computes the same
+    climate indices by a moving window approach over the entire dataset. That is, for each time step, the climate
+    indices are re-computed from the last `window_length` time steps.
+
+    Parameters
+    ----------
+    precip : pd.Series
+        Time-indexed series of precipitation.
+    tmax : pd.Series
+        Time-indexed series of maximum temperature.
+    tmin : pd.Series
+        Time-indexed series of minimum temperature.
+    pet : pd.Series
+        Time-indexed series of potential evapotranspiration.
+    window_length : int
+        Look-back period to use to compute the climate indices.
+    raise_nan : bool, optional
+        If True, will raise a ValueError if a climate index is NaN. Default: False.
+
+    Returns
+    -------
+    pd.DataFrame
+        Time-indexed DataFrame of climate indices. By definition, the climate indices for a given day in the
+        DataFrame are computed from the `window_length` previous time steps (including the given day).
+
+    Raises
+    ------
+    ValueError
+        If `raise_nan` is True and a calculated climate index is NaN at any point in time.
+    """
+    x = np.array([precip.values, tmax.values, tmin.values, pet.values]).T
+
+    new_features = _numba_climate_indexes(x, window_length=window_length)
+
+    df = pd.DataFrame(
+        {
+            'p_mean_dyn': new_features[:, 0],
+            'pet_mean_dyn': new_features[:, 1],
+            'aridity_dyn': new_features[:, 2],
+            't_mean_dyn': new_features[:, 3],
+            'frac_snow_dyn': new_features[:, 4],
+            'high_prec_freq_dyn': new_features[:, 5],
+            'high_prec_dur_dyn': new_features[:, 6],
+            'low_prec_freq_dyn': new_features[:, 7],
+            'low_prec_dur_dyn': new_features[:, 8]
+        },
+        index=precip.iloc[window_length - 1:].index)
+
+    if raise_nan and np.any(df.isna()):
+        raise ValueError(f"NaN in climate indices {[col for col in df.columns[df.isna().any()]]}")
+
+    return df
+
+
 @njit
 def _numba_climate_indexes(features: np.ndarray, window_length: int) -> np.ndarray:
+    # features shape is (#timesteps, 4), where 4 breaks down into: (prcp, tmax, tmin, pet)
     n_samples = features.shape[0]
-    new_features = np.zeros((n_samples - 365 + 1, 9))
+    new_features = np.zeros((n_samples - window_length + 1, 9))
 
     for i in range(new_features.shape[0]):
         x = features[i:i + window_length]
@@ -112,11 +171,15 @@ def _numba_climate_indexes(features: np.ndarray, window_length: int) -> np.ndarr
         aridity = pet_mean / p_mean
         t_mean = (np.mean(x[:, 1]) + np.mean(x[:, 2])) / 2
 
-        precip_days = x[x[:, 0] > 0]
-        frac_snow = precip_days[precip_days[:, 2] < 0, :].shape[0] / precip_days.shape[0]
+        # fraction of precipitation falling as snow
+        if np.sum(x[:, 0]) > 0:
+            mean_temp = (x[:, 1] + x[:, 2]) / 2
+            frac_snow = np.sum(x[mean_temp <= 0, 0]) / np.sum(x[:, 0])
+        else:
+            frac_snow = 0.0
 
-        high_prec_freq = precip_days[precip_days[:, 0] > 5 * p_mean].shape[0] / precip_days.shape[0]
-        low_prec_freq = precip_days[precip_days[:, 0] < 1].shape[0] / precip_days.shape[0]
+        high_prec_freq = np.sum(x[:, 0] >= 5 * p_mean) / x.shape[0]
+        low_prec_freq = np.sum(x[:, 0] < 1) / x.shape[0]
 
         idx = np.where(x[:, 0] < 1)[0]
         groups = _split_list(idx)
