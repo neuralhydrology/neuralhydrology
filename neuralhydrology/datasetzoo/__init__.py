97dc2ae0cad7a794226c83b933962a15bb73d90f
vcloarec <vcloarec@gmail.com>
2025-02-10 08:47:01 +0000

    docstring

--- neuralhydrology/datasetzoo/__init__.py
+++ neuralhydrology/datasetzoo/__init__.py
@@ -69,17 +69,16 @@ def get_dataset(cfg: Config,
 
 
 def register_dataset(key: str, new_class: Type):
-    """Register a datatet class that can be used by neuralhydrology. 
-
+    """Register a dataset class for use with neuralhydrology.
     This class must derive from BaseDataset. New dataset calss has to be added at the begining of runtime.
 
     Parameters
     ----------
-    key : str 
+    key : str
         the key of the dataset that is set in the configuration file.
 
-    new_class : Type 
-        The new Dataset class to register 
+    new_class : Type
+        The new Dataset class to register
 
     Returns
     -------
@@ -97,7 +96,7 @@ def register_dataset(key: str, new_class: Type):
 class DatasetZooRegistry:
     """Class that register dataset classes taht can be used with neuralhydrology.
 
-       This class is an implementation detatil that must not be used outside this file.
+       This class is an implementation detail that must not be used outside this file.
     """
 
     def __init__(self):
@@ -116,6 +115,30 @@ class DatasetZooRegistry:
         self.__dataset_class["caravan"] = Caravan
 
     def register_dataset_class(self, key: str, new_class: Type):
+        """Register a new dataset class in the dataset zoo registry.
+
+        Parameters
+        ----------
+        key : str
+            The unique identifier for the dataset class. This key will be used in configuration files
+            to specify which dataset to use.
+        new_class : Type
+            The dataset class to register. Must be a subclass of BaseDataset.
+
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        TypeError
+            If the provided class is not a subclass of BaseDataset.
+
+        Examples
+        --------
+        >>> registry = DatasetZooRegistry()
+        >>> registry.register_dataset_class("my_dataset", MyCustomDataset)
+        """
         if issubclass(new_class, BaseDataset):
             self.__dataset_class[key] = new_class
         else:
@@ -128,7 +151,44 @@ class DatasetZooRegistry:
                             basin: str = None,
                             additional_features: list = [],
                             id_to_int: dict = {},
-                            scaler: dict = {}):
+                            scaler: dict = {}) -> BaseDataset:
+        """Create and return an instance of a dataset class based on the configuration.
+
+        Parameters
+    ----------
+    cfg : Config
+        The run configuration.
+    is_train : bool
+        Defines if the dataset is used for training or evaluating. If True (training), means/stds for each feature
+        are computed and stored to the run directory. If one-hot encoding is used, the mapping for the one-hot encoding
+        is created and also stored to disk. If False, a `scaler` input is expected and similarly the `id_to_int` input
+        if one-hot encoding is used.
+    period : {'train', 'validation', 'test'}
+        Defines the period for which the data will be loaded
+    basin : str, optional
+        If passed, the data for only this basin will be loaded. Otherwise the basin(s) is(are) read from the appropriate
+        basin file, corresponding to the `period`.
+    additional_features : List[Dict[str, pd.DataFrame]], optional
+        List of dictionaries, mapping from a basin id to a pandas DataFrame. This DataFrame will be added to the data
+        loaded from the dataset and all columns are available as 'dynamic_inputs', 'evolving_attributes' and
+        'target_variables'
+    id_to_int : Dict[str, int], optional
+        If the config argument 'use_basin_id_encoding' is True in the config and period is either 'validation' or
+        'test', this input is required. It is a dictionary, mapping from basin id to an integer (the one-hot encoding).
+    scaler : Dict[str, Union[pd.Series, xarray.DataArray]], optional
+        If period is either 'validation' or 'test', this input is required. It contains the centering and scaling
+        for each feature and is stored to the run directory during training (train_data/train_data_scaler.yml).
+
+        Returns
+        -------
+        BaseDataset
+            An instance of the appropriate dataset class based on the configuration.
+
+        Raises
+        ------
+        NotImplementedError
+            If no dataset class is implemented for the dataset specified in the configuration.
+        """
         dataset_key = cfg.dataset.lower()
         Dataset = self.__dataset_class.get(dataset_key, None)
         if Dataset is None:
